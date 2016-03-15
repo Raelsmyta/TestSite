8a193a1b96f564f91f9896c1a059c57b4f53d16a
Raelsmyta <Raelsmyta@sou.edu>
2016-03-15 04:49:35 +0000

    basic site

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
   get 'order_items/create'
-
   get 'order_items/update'
-
   get 'order_items/destroy'
-
   get 'carts/show'
-
   get 'products/index'
 
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+
   resources :products, only: [:index]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
-  root to: "products#index"
+  root to: "welcome#index"
 end
 end
